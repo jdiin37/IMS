39ea3538544df1d8c792e41def8acefb1cbf865a
Matt Kuo <jdiin37@gmail.com>
2018-09-19 16:28:55 +0800

    remove 2 lib

---
+++ IMS.Data/BaseEntities/IMSEntities.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Data.Entity.Validation;
+using System.Linq;
+
+namespace IMS.Data
+{
+    public partial class IMSEntities : DbContext
+    {
+        public IMSEntities()
+            : base("IMSEntities")
+        {
+            this.Configuration.LazyLoadingEnabled = true;
+        }
+    }
+}
---
+++ IMS.Data/Entities/Account.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace IMS.Data
+{
+    class Account
+    {
+       
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [MaxLength(20)]
+        public string UserNo { get; set; }
+
+        [MaxLength(100)]
+        public string Email { get; set; }
+
+        [MaxLength(50)]
+        public string Password { get; set; }
+
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        //    public int? Title_id { get; set; }
+        public string Title { get; set; }
+        [MaxLength(1)]
+        public string Gender { get; set; }
+
+        public DateTime? Birthday { get; set; }
+
+        [MaxLength(30)]
+        public string Tel { get; set; }
+
+        [MaxLength(1)]
+        public string IsLockedOut { get; set; }
+
+        [MaxLength(1)]
+        public string IsDoctor { get; set; }
+
+        public string ImagePath { get; set; }
+
+        [Column(TypeName = "ntext")]
+        public string Comment { get; set; }
+
+        [Column(TypeName = "ntext")]
+        public string Experience { get; set; }
+        [Column(TypeName = "ntext")]
+        public string Major { get; set; }
+        [Column(TypeName = "ntext")]
+        public string Expertise { get; set; }
+        [MaxLength(50)]
+        public string Status { get; set; }
+
+        public DateTime? LastLoginDate { get; set; }
+
+        public DateTime CreateDate { get; set; }
+
+        public DateTime ModDate { get; set; }
+
+        [MaxLength(100)]
+        public string ModUser { get; set; }
+    }
+}
