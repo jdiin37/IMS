e8b55644b0d4d2425dfa846b61df2062fafece0b
matt <matt@advmeds.com>
2018-09-20 11:36:06 +0800

    Add Login Page

---
+++ IMS/Controllers/LoginController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IMS.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Login()
+        {
+
+
+
+
+            if(true)
+            {
+                return RedirectToAction("Index","Main");
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+            
+        }
+    }
+}
