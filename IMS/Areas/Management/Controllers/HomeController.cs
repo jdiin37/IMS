5178a69a692855e6e2ca413cd750d456f6332dbe
jdiin37 <jdiin37@gmail.com>
2019-01-03 18:20:12 +0800

    add FarmDataBase Ajax

--- IMS/Areas/Management/Controllers/HomeController.cs
+++ IMS/Areas/Management/Controllers/HomeController.cs
@@ -1,17 +1,36 @@
-﻿using System;
+﻿using IMS.Controllers;
+using IMS.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 
 namespace IMS.Areas.Management.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
         // GET: Management/Home
         public ActionResult Index()
         {
             return View();
+        }
+        
+        public ActionResult GetFarmDataBase(Guid pigFarmId)
+        {
+            FarmDataBase farmDataBase = IMSdb.FarmDataBase.Where(m => m.PigFarmId ==pigFarmId).FirstOrDefault();
+            if (Request.IsAjaxRequest())
+            {
+                if (farmDataBase == null)
+                {
+                    return Json(farmDataBase, JsonRequestBehavior.AllowGet);
+                }
+                
+                return Json(farmDataBase, JsonRequestBehavior.AllowGet);  //將物件序列化JSON並回傳
+            }
+
+            return Json(farmDataBase, JsonRequestBehavior.AllowGet);
         }
     }
 }
