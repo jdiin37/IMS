a75ea6b916011cafe2eb5d040eaf05e531a95146
jdiin37 <jdiin37@gmail.com>
2019-03-28 18:17:33 +0800

    add pigletGrow

---
+++ IMS/Areas/Traceability/Controllers/PigletGrowController.cs
@@ -0,0 +1,44 @@
+﻿using IMS.Comm;
+using IMS.ViewModels;
+using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IMS.Areas.Traceability.Controllers
+{
+  public class PigletGrowController : Controller
+  {
+    // GET: Traceability/PigletGrow
+    public ActionResult Index(int? page)
+    {
+      var list = getPigletGrows();
+
+      int pageNumber = (page ?? 1);
+      return View(list.ToPagedList(pageNumber, Config.PageSize));
+    }
+
+    private IEnumerable<PigletGrow> getPigletGrows()
+    {
+      List<PigletGrow> list = new List<PigletGrow> (){
+        new PigletGrow
+        {
+          BornDate_s = new DateTime(2018,01,01),
+          BornDate_e = new DateTime(2018,01,08),
+          PigletCnt = 120,
+        },
+        new PigletGrow
+        {
+          BornDate_s = new DateTime(2018,01,09),
+          BornDate_e = new DateTime(2018,01,16),
+          PigletCnt = 114,
+        },
+      };
+      
+      return list.ToList();
+
+    }
+  }
+}
---
+++ IMS/ViewModels/PigletGrow.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IMS.ViewModels
+{
+  public class PigletGrow
+  {
+    public DateTime BornDate_s { get; set; }
+
+    public DateTime BornDate_e { get; set; }
+
+    public int PigletCnt { get; set; }
+
+
+
+
+  }
+}
