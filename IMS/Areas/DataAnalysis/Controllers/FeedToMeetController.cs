ce4f1066c3978e4e6f236b81877c7fb226c6c938
jdiin37 <jdiin37@gmail.com>
2019-06-14 00:26:20 +0800

    Update FeedToMeetController.cs

--- IMS/Areas/DataAnalysis/Controllers/FeedToMeetController.cs
+++ IMS/Areas/DataAnalysis/Controllers/FeedToMeetController.cs
@@ -187,56 +187,69 @@ namespace IMS.Areas.DataAnalysis.Controllers
         feetToMeet.Stage3ePigCnt = pigCnt;
         feetToMeet.Stage4sPigCnt = pigCnt;
         feetToMeet.Stage4ePigCnt = pigCnt;
+        
+        
+        feetToMeet.Stage1AddWeight = Math.Round(pigCnt * getRandomDouble(0.18, 0.24), 2) * feetToMeet.Stage1Days;
+        if (feetToMeet.SowType == "L")
+        {
+          feetToMeet.Stage2AddWeight = Math.Round(pigCnt * getRandomDouble(0.40, 0.46), 2) * feetToMeet.Stage2Days;
+        }
+        else
+        {
+          feetToMeet.Stage2AddWeight = Math.Round(pigCnt * getRandomDouble(0.37, 0.42), 2) * feetToMeet.Stage2Days;
+        }
+        if (feetToMeet.SowType == "L")
+        {
+          feetToMeet.Stage3AddWeight = Math.Round(pigCnt * getRandomDouble(0.70, 0.82), 2) * feetToMeet.Stage3Days;
+        }
+        else
+        {
+          feetToMeet.Stage3AddWeight = Math.Round(pigCnt * getRandomDouble(0.65, 0.77), 2) * feetToMeet.Stage3Days;
+        }
+
+        feetToMeet.Stage4AddWeight = Math.Round(pigCnt * getRandomDouble(0.95, 1.13), 2) * feetToMeet.Stage4Days;
+
+
 
         feetToMeet.Stage1sWeight = 0;
-        feetToMeet.Stage1eWeight = Math.Round(pigCnt * getRandomDouble(5, 7), 2);
+        feetToMeet.Stage1eWeight = feetToMeet.Stage1AddWeight;
         feetToMeet.Stage2sWeight = feetToMeet.Stage1eWeight;
-        if (feetToMeet.SowType == "L")
-        {
-
-          feetToMeet.Stage2eWeight = Math.Round(pigCnt * getRandomDouble(12, 20), 2);
-        }
-        else
-        {
-          feetToMeet.Stage2eWeight = Math.Round(pigCnt * getRandomDouble(12, 24), 2);
-        }
+        feetToMeet.Stage2eWeight = feetToMeet.Stage2sWeight + feetToMeet.Stage2AddWeight;
         feetToMeet.Stage3sWeight = feetToMeet.Stage2eWeight;
-        if (feetToMeet.SowType == "L")
-        {
-
-          feetToMeet.Stage3eWeight = Math.Round(pigCnt * getRandomDouble(40, 60), 2);
-        }
-        else
-        {
-          feetToMeet.Stage3eWeight = Math.Round(pigCnt * getRandomDouble(44, 68), 2);
-        }
-        
+        feetToMeet.Stage3eWeight = feetToMeet.Stage3sWeight + feetToMeet.Stage3AddWeight;
         feetToMeet.Stage4sWeight = feetToMeet.Stage3eWeight;
-        if (feetToMeet.SowType == "L")
+        feetToMeet.Stage4eWeight = feetToMeet.Stage4sWeight + feetToMeet.Stage4AddWeight;
+
+
+        if(pigCnt > 23)
         {
-          feetToMeet.Stage4eWeight = Math.Round(pigCnt * getRandomDouble(60, 110), 2);
+          feetToMeet.Stage1FeedWeight = Math.Round(feetToMeet.Stage1ePigCnt * 0.28 * feetToMeet.Stage1Days, 2);
+          feetToMeet.Stage2FeedWeight = Math.Round(feetToMeet.Stage2ePigCnt * 0.75 * feetToMeet.Stage2Days, 2);
+          feetToMeet.Stage3FeedWeight = Math.Round(feetToMeet.Stage3ePigCnt * 2.25 * feetToMeet.Stage3Days, 2);
+          feetToMeet.Stage4FeedWeight = Math.Round(feetToMeet.Stage4ePigCnt * 3.7 * feetToMeet.Stage4Days, 2);
         }
         else
         {
-          feetToMeet.Stage4eWeight = Math.Round(pigCnt * getRandomDouble(74, 120), 2);
+          feetToMeet.Stage1FeedWeight = Math.Round(feetToMeet.Stage1ePigCnt * 0.20 * feetToMeet.Stage1Days, 2);
+          feetToMeet.Stage2FeedWeight = Math.Round(feetToMeet.Stage2ePigCnt * 0.67 * feetToMeet.Stage2Days, 2);
+          feetToMeet.Stage3FeedWeight = Math.Round(feetToMeet.Stage3ePigCnt * 2.05 * feetToMeet.Stage3Days, 2);
+          feetToMeet.Stage4FeedWeight = Math.Round(feetToMeet.Stage4ePigCnt * 3.6 * feetToMeet.Stage4Days, 2);
         }
-        
-        feetToMeet.Stage1AddWeight = feetToMeet.Stage1eWeight - feetToMeet.Stage1sWeight;
-        feetToMeet.Stage2AddWeight = feetToMeet.Stage2eWeight - feetToMeet.Stage2sWeight;
-        feetToMeet.Stage3AddWeight = feetToMeet.Stage3eWeight - feetToMeet.Stage3sWeight;
-        feetToMeet.Stage4AddWeight = feetToMeet.Stage4eWeight - feetToMeet.Stage4sWeight;
-
-        feetToMeet.Stage1FeedWeight = Math.Round(feetToMeet.Stage1ePigCnt * 0.28 * feetToMeet.Stage1Days, 2);
-        feetToMeet.Stage2FeedWeight = Math.Round(feetToMeet.Stage2ePigCnt * 0.67 * feetToMeet.Stage2Days, 2);
-        feetToMeet.Stage3FeedWeight = Math.Round(feetToMeet.Stage3ePigCnt * 1.63 * feetToMeet.Stage3Days, 2);
-        feetToMeet.Stage4FeedWeight = Math.Round(feetToMeet.Stage4ePigCnt * 3.4 * feetToMeet.Stage4Days, 2);
 
         feetToMeet.Stage1FeedToMeet = Math.Round(feetToMeet.Stage1FeedWeight/ feetToMeet.Stage1AddWeight, 2);
         feetToMeet.Stage2FeedToMeet = Math.Round(feetToMeet.Stage2FeedWeight / feetToMeet.Stage2AddWeight, 2);
         feetToMeet.Stage3FeedToMeet = Math.Round(feetToMeet.Stage3FeedWeight / feetToMeet.Stage3AddWeight, 2);
         feetToMeet.Stage4FeedToMeet = Math.Round(feetToMeet.Stage4FeedWeight / feetToMeet.Stage4AddWeight, 2);
 
+        //if((feetToMeet.Stage1FeedToMeet > 0.7 && feetToMeet.Stage1FeedToMeet < 1.4) &&
+        //  (feetToMeet.Stage2FeedToMeet > 1.0 && feetToMeet.Stage2FeedToMeet < 1.6) &&
+        //  (feetToMeet.Stage3FeedToMeet > 1.8 && feetToMeet.Stage3FeedToMeet < 3.2) &&
+        //  (feetToMeet.Stage4FeedToMeet > 2.8 && feetToMeet.Stage4FeedToMeet < 4.2)
+        //  )
+        //{
+        //}
         IMSdb.FeetToMeetData.Add(feetToMeet);
+
       }
 
       IMSdb.SaveChanges();
